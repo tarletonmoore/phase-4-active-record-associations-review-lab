39c6eee1d4acfe3ae598a50a60aa579839ec3a5c
John Tarleton Moore <tarletonmoore@gmail.com>
2022-11-28 12:27:17 -0500

    commit tree 816273d74b5600d4449f22c14cf43dcda303b50e

--- db/migrate/20210506153121_create_passengers.rb
+++ db/migrate/20210506153121_create_passengers.rb
@@ -1,7 +1,7 @@
 class CreatePassengers < ActiveRecord::Migration[6.1]
   def change
     create_table :passengers do |t|
-
+# t.belongs_to :taxi, null: false, foreign_key: true
       t.timestamps
     end
   end
